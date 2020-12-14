4fe13181eb4c9d9a62057c1ed0ba8dbc67e1cfb7
Lindsey <lindsey.neri18@gmail.com>
2020-12-14 15:10:13 -0500

    Assignment_3

---
+++ Assignment 3/Assignment_3.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    def cylinder_volume():
+        r = int(input("Enter radius of cylinder:"))
+        h = int(input("Enter height of cylinder:"))
+        r_squared = r * r
+        volume = r_squared * h * 3.14
+        print("The volume of the cylinder is ", volume)
+cylinder_volume()
+
+if __name__ == "__main__":
+    def square_root_function():
+        x = int(input("Enter the number to et the square root of"))
+        x_squared = x * x
+        print("The square root of ", x, "is ", x_squared)
+square_root_function()
